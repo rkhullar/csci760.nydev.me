09dacceeced2e35634dfe32ef6e1aaf0eae54db8
Rajan Khullar <rkhullar@nyit.edu>
2016-04-10 19:48:51 -0400

    dev setup

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -2,8 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
+@app.route('/')
+def index():
+    return '<h2>Hello, I love Digital Ocean!</h2>'
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return '<h2>Hello, %s</h2>' % username
+
+
+@app.route('/post/<int:id>')
+def post(id):
+    return '<h2>post id = %d</h2>' % id
+
 if __name__ == "__main__":
     app.run()
---
+++ keep/wsgi.py
@@ -0,0 +1,8 @@
+import sys
+import logging
+
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, "/srv/nydev/csci760.nydev.me")
+
+from FlaskApp import app as application
+application.secret_key = 'Jx48YpWqp36395M198tT9D68pasbBGEj'
