6f14cdc76c9d13e295cfe93da018677bd041d7ff
Rajan Khullar <rkhullar@nyit.edu>
2016-04-27 23:43:35 -0400

    dev reader

--- database/inflate.sql
+++ database/inflate.sql
@@ -1,12 +1,12 @@
 /*
  * @author  : Rajan Khullar
  * @created : 04/16/16
- * @updated : 04/23/16
+ * @updated : 04/27/16
  */
 
 truncate dbo.actor cascade;
 select new.actor('Admin', 'Admin', 'admin@nydev.me', digest('mypass', 'sha256'));
-select new.actor('Rajan', 'Khullar', 'rajan@nydev.me');
+select new.reader('Rajan', 'Khullar', 'rajan@nydev.me', 12345678);
 
 truncate dbo.admin;
 insert into dbo.admin
--- database/rebuild.sql
+++ database/rebuild.sql
@@ -1,15 +1,17 @@
 /*
  * @author  : Rajan Khullar
  * @created : 04/16/16
- * @updated : 04/23/16
+ * @updated : 04/27/16
  */
 
 create extension if not exists pgcrypto;
 
 drop schema if exists dbo cascade;
+drop schema if exists dbv cascade;
 drop schema if exists new cascade;
 
 create schema dbo;
+create schema dbv;
 create schema new;
 
 create table dbo.actor
@@ -26,9 +28,28 @@ create table dbo.admin
   id serial references dbo.actor(id)
 );
 
-create function new.actor(varchar(20), varchar(20), varchar(50), bytea default digest('aaaaaa', 'sha256'))
-  returns boolean as $$
+create table dbo.reader
+(
+  id serial references dbo.actor(id),
+  card int8 unique not null
+);
+
+create view dbv.reader as
+  select a.id, a.firstname, a.lastname, r.card
+  from dbo.actor a, dbo.reader r
+  where a.id = r.id;
+
+create function new.actor(fname varchar(20), lname varchar(20), email varchar(50), pswd bytea default digest('aaaaaa', 'sha256'))
+  returns integer as $$
 insert into dbo.actor(firstname, lastname, email, "password")
-values ($1, $2, $3, $4);
-select true as result;
+  values ($1, $2, $3, $4)
+  returning id;
 $$ language sql;
+
+
+create function new.reader(fname varchar(20), lname varchar(20), email varchar(50), card int8, pswd bytea default digest('aaaaaa', 'sha256'))
+  returns integer as $$
+  insert into dbo.reader(id, card)
+      values (new.actor($1, $2, $3, $5), $4)
+      returning id;
+  $$ language sql;
